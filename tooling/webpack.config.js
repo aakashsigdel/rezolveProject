1ae4a63f5ff71f3291e7a98594181067a389b59e
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-12-10 08:18:21 +0545

    initial commit with configs

---
+++ tooling/webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var packagejson = require('../package.json');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    path.resolve(__dirname, '../src/index.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        include: path.resolve(__dirname, '../src')
+      }, {
+        test: /\.css$/, // Only .css files
+        loader: 'style!css' // Run both loaders
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../src/index.html'),
+      inject: 'body', // Inject webpack scripts into the body.
+      hash: true
+    }),
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(packagejson.version)
+    })
+  ],
+
+  resolve: {
+    root: path.resolve(__dirname, '../src')
+  }
+};
