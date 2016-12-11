d2cf5ce7fa85c10b491a7e636f8cd79c9210d474
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-12-11 19:30:40 +0545

    add mocks for styles and assets; add test for Header

---
+++ __tests__/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../src/components/Header'
+
+import renderer from 'react-test-renderer'
+
+it('should render <Header /> correctly', () => {
+  const tree = renderer.create(
+    <Header />
+  ).toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
