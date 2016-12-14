da47b0cf9d8ff4fb394cf99a9a149041014a4157
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-12-14 19:26:43 +0545

    add persistance

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import { loadState, saveState } from './localStorage'
 
- const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-  ))
+const persistedState = loadState()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(thunk)))
+
+store.subscribe(() => {
+  saveState({
+    mybooks: store.getState().mybooks
+  })
+})
 
 export default store
